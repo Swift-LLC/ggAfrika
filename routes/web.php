603acfc0e6de6f40a69ec0eda22c97e6cedb88a7
Edwin Odhiambo <wawinyedwin44@gmail.com>
2022-01-23 12:24:41 +0300

    middleware removal

--- routes/web.php
+++ routes/web.php
@@ -35,15 +35,15 @@ Route::get('/category/{category}', [App\Http\Controllers\PostsController::class,
 Route::get('/admin/calendar', function () {
     return view('admin.calendar');
 })->name('calendar')->middleware('is_admin');
-Route::get('/blog/create/post', [App\Http\Controllers\PostsController::class, 'create'])->name('create')->middleware(isAdmin::class);//shows create post form
-Route::post('/store', [App\Http\Controllers\PostsController::class, 'store'])->name('store')->middleware('is_admin');//saves the created post to thed database
-Route::get('/{post}/edit', [App\Http\Controllers\PostsController::class, 'edit'])->name('edit')->middleware('is_admin');//shows edit post form
-Route::put('/blog/{post}/post', [App\Http\Controllers\PostsController::class, 'update'])->name('update')->middleware('is_admin');//commits edit posts to the database
-Route::delete('/delete/{post}', [App\Http\Controllers\PostsController::class, 'destroy'])->name('delete')->middleware('is_admin');//delete posts from database
-Route::post('/store/post', [App\Http\Controllers\CategoryController::class, 'store'])->name('cat')->middleware('is_admin');
-Route::get('/posts', [App\Http\Controllers\Admin\PostController::class, 'showPosts'])->name('posts')->middleware('is_admin');
-Route::put('/blog/{post}/publish', [App\Http\Controllers\PostsController::class, 'publish'])->name('publish')->middleware('is_admin');
-Route::put('/video/{post}/publish', [App\Http\Controllers\VideoController::class, 'publish'])->name('v_publish')->middleware('is_admin');
+Route::get('/blog/create/post', [App\Http\Controllers\PostsController::class, 'create'])->name('create');//shows create post form
+Route::post('/store', [App\Http\Controllers\PostsController::class, 'store'])->name('store');//saves the created post to thed database
+Route::get('/{post}/edit', [App\Http\Controllers\PostsController::class, 'edit'])->name('edit');//shows edit post form
+Route::put('/blog/{post}/post', [App\Http\Controllers\PostsController::class, 'update'])->name('update');//commits edit posts to the database
+Route::delete('/delete/{post}', [App\Http\Controllers\PostsController::class, 'destroy'])->name('delete');//delete posts from database
+Route::post('/store/post', [App\Http\Controllers\CategoryController::class, 'store'])->name('cat');
+Route::get('/posts', [App\Http\Controllers\Admin\PostController::class, 'showPosts'])->name('posts');
+Route::put('/blog/{post}/publish', [App\Http\Controllers\PostsController::class, 'publish'])->name('publish');
+Route::put('/video/{post}/publish', [App\Http\Controllers\VideoController::class, 'publish'])->name('v_publish');
 
 Auth::routes();
 
